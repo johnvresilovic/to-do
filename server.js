252c5642eff43b16d336819edcbc7dc7c05010d8
John Vresilovic <johnvresilovic@protonmail.com>
2023-01-13 13:51:38 -0500

    appears to work

--- server.js
+++ server.js
@@ -2,89 +2,46 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const toDos = require("./models/todos");
+const cors = require("cors")
+const todosController = require("./controllers/todos.js")
+const todosData = require('./utilities/data')
 
-//Environmental variables
+
+// Environmental variables
 const app = express();
 const mongoURI = process.env.MONGO_URI;
 const port = process.env.port || 3001;
 const db = mongoose.connection;
 
-//Connectign to MongoDB
+// Connectign to MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("strictQuery", true);
 mongoose.connection.once("open", () => {
   console.log("bongo mongo");
 });
 
-//Middleware
-app.use((req, res, next) => {
-  console.log("I run for Iran...");
-  next();
-});
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
+// Error / Disconnection
+db.on('error', err => console.log(err.message + ' is Mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
-//Root
-app.get("/", (req, res) => {
-  res.render("Home");
-});
+// Middleware
+app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
+app.use(express.json()); //use .json(), not .urlencoded()
+app.use(express.static('public')) // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
+app.use(cors())
 
-//Index
-app.get("/todo", (req, res) => {
-  todo.find({}, (error, allTodos) => {
-    res.render("Index", {
-      todo: allTodos,
-    });
+// Routes
+app.use("/todos", todosController) 
+// telling server.js to get the routes from controller/todos.js
+
+// Seeding the db
+app.get('/seed', async (req, res) => {
+    await toDos.deleteMany({});
+    await toDos.insertMany(todosData);
+    res.send('done!');
   });
-});
 
-//New
-app.get("/todo/new", (req, res) => {
-  res.render("New");
-});
-
-//Delete
-app.delete("/todo/:id", (req, res) => {
-  todo.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect("/todo");
-  });
-});
-
-//Update
-app.put("/todo/:todoId", (req, res) => {
-  
-});
-
-//Create (or Add)
-app.post("/todo", (req, res) => {
-  todo.create(req.body, (error, createdTodo) => {
-    res.redirect("/todo");
-  });
-});
-
-//Edit
-app.get("/todo/:id/edit", (req, res) => {
-  todo.findById(req.params.id, (err, foundTodo) => {
-    if (!err) {
-      res.render("Edit", {
-        todo: foundTodo,
-      });
-    } else {
-      res.send({ msg: err.message });
-    }
-  });
-});
-
-//Show
-app.get("/todo/:todoId", function (req, res) {
-  todo.findById(req.params.todoId, (err, foundTodo) => {
-    res.render("Show", {
-        todo: foundTodo,
-    });
-  });
-});
-
-//Port
 app.listen(port, () => {
-  console.log("Deathrace", port);
-});
+    console.log('This message means nothing', port)
+  })
